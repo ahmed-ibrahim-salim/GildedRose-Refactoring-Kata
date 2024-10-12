14a34951d785d5b5625205ff696060ac60054ed2
Ahmed Ibrahim <ahmed.ibrahim@efreshli.com>
2024-10-12 14:08:38 +0300

    changed if to switch

--- Sources/GildedRose/GildedRose.swift
+++ Sources/GildedRose/GildedRose.swift
@@ -7,50 +7,64 @@ public class GildedRose {
 
     public func updateQuality() {
         for i in 0 ..< items.count {
-            if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                if items[i].quality > 0 {
-                    if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                        items[i].quality = items[i].quality - 1
+            let item = items[i]
+            appleSauce(item)
+        }
+    }
+
+    func appleSauce(_ item: Item) {
+        switch item.name {
+        case "Aged Brie":
+            if item.quality < 50 {
+                item.quality = item.quality + 1
+            }
+
+            item.sellIn = item.sellIn - 1
+
+            if item.sellIn < 0 {
+                if item.quality < 50 {
+                    item.quality = item.quality + 1
+                }
+            }
+        case "Backstage passes to a TAFKAL80ETC concert":
+            if item.quality < 50 {
+                item.quality = item.quality + 1
+
+                if item.sellIn < 11 {
+                    if item.quality < 50 {
+                        item.quality = item.quality + 1
                     }
                 }
-            } else {
-                if items[i].quality < 50 {
-                    items[i].quality = items[i].quality + 1
 
-                    if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].sellIn < 11 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if items[i].sellIn < 6 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
+                if item.sellIn < 6 {
+                    if item.quality < 50 {
+                        item.quality = item.quality + 1
                     }
                 }
             }
 
-            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                items[i].sellIn = items[i].sellIn - 1
+            item.sellIn = item.sellIn - 1
+
+            if item.sellIn < 0 {
+                item.quality = 0
+            }
+        case "Sulfuras, Hand of Ragnaros":
+            break
+        default:
+            if item.quality > 0 {
+                if item.name != "Sulfuras, Hand of Ragnaros" {
+                    item.quality = item.quality - 1
+                }
             }
 
-            if items[i].sellIn < 0 {
-                if items[i].name != "Aged Brie" {
-                    if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].quality > 0 {
-                            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if items[i].quality < 50 {
-                        items[i].quality = items[i].quality + 1
+            if item.name != "Sulfuras, Hand of Ragnaros" {
+                item.sellIn = item.sellIn - 1
+            }
+
+            if item.sellIn < 0 {
+                if item.quality > 0 {
+                    if item.name != "Sulfuras, Hand of Ragnaros" {
+                        item.quality = item.quality - 1
                     }
                 }
             }
